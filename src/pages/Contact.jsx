e82b00a28113a3c020be1d868e2f36f80dc02dc0
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-07-24 22:53:14 +0530

    added abouot and contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='w-full h-[80vh] text-9xl flex justify-center items-center text-gray-400 uppercase'>This Is Contact Page</div>
+    <div className='w-full h-[70vh]'>
+      <h1 className='text-3xl mt-20 ms-20 text-center'>We Are Available At</h1>
+      <div className='gap-10 flex flex-wrap m-auto justify-center mt-20 text-stone-700'>
+                <p className='ri-facebook-fill text-8xl'></p>
+                <p className='ri-instagram-line text-8xl'></p>
+                <p className='ri-pinterest-line text-8xl'></p>
+                <p className='ri-twitter-fill text-8xl'></p>
+                <p className='ri-tiktok-fill text-8xl'></p>
+                <p className='ri-mail-line text-8xl'></p>
+            </div>
+    </div>
   )
 }
 
